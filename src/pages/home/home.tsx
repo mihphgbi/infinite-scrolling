c0e4d3116747e2ba4f45a20f7a84e9afc159d0fd
anna <anna.bui@pal.net.vn>
2024-02-01 16:02:50 +0700

    feat: add some style

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react';
+import React, {useEffect, useRef, useState} from 'react';
 import axios from 'axios';
 import {Products} from "../../models/Product";
 import "../home/styles/style.scss";
@@ -9,48 +9,53 @@ const HomePage: React.FC = () =>  {
     const [isLoading, setIsLoading] = useState(false);
     const [searchKeyword, setSearchKeyword] = useState('');
     const [totalItem, setTotalItem] = useState(0);
+    const isMounted = useRef(false);
 
     useEffect(() => {
-        const getListOfProduct = async () => {
-            setIsLoading(true)
-            try {
-                let url = `https://dummyjson.com/products?limit=20&skip=${skip}`;
-                if (searchKeyword) {
-                    url = `https://dummyjson.com/products/search?q=${searchKeyword}&limit=20&skip=${skip}`;
+        if (isMounted.current) {
+            const handleScroll = () => {
+                if ((Math.ceil(window.innerHeight + document.documentElement.scrollTop) === Math.ceil(document.documentElement.offsetHeight)) ||
+                    (Math.floor(window.innerHeight + document.documentElement.scrollTop) === Math.floor(document.documentElement.offsetHeight))) {
+                    skip <= totalItem && setSkip(prevPage => prevPage + 20);
+                } else {
+                    return;
                 }
-                const res = await axios.get(url)
-                if(res.data.products.length > 0) {
-                    setDataRender(prevData => [...prevData, ...res.data.products]);
-                    setTotalItem(res.data.total)
-                }
-                return res.data.products;
-            } catch (error: any) {
-                console.debug("error", error.response.data)
-            }finally {
-                setIsLoading(false);
-            }
+            };
+            window.addEventListener('scroll', handleScroll);
+
+            getListOfProduct();
+
+            return () => {
+                window.removeEventListener('scroll', handleScroll);
+            };
+        } else {
+            isMounted.current = true;
         }
-        const handleScroll = () => {
-            if ((Math.ceil(window.innerHeight + document.documentElement.scrollTop) === Math.ceil(document.documentElement.offsetHeight)) ||
-                (Math.floor(window.innerHeight + document.documentElement.scrollTop) === Math.floor(document.documentElement.offsetHeight))) {
-                skip <= totalItem && setSkip(prevPage => prevPage + 20);
-            } else {
-                return;
-            }
-        };
-        window.addEventListener('scroll', handleScroll);
-
-        getListOfProduct();
-
-        return () => {
-            window.removeEventListener('scroll', handleScroll);
-        };
     }, [skip, searchKeyword]);
 
+    const getListOfProduct = async () => {
+        setIsLoading(true)
+        try {
+            let url = `https://dummyjson.com/products?limit=20&skip=${skip}`;
+            if (searchKeyword) {
+                url = `https://dummyjson.com/products/search?q=${searchKeyword}&limit=20&skip=${skip}`;
+            }
+            const res = await axios.get(url)
+            if(res.data.products.length > 0) {
+                setDataRender(prevData => [...prevData, ...res.data.products]);
+                setTotalItem(res.data.total)
+            }
+            return res.data.products;
+        } catch (error: any) {
+            console.debug("error", error.response.data)
+        }finally {
+            setIsLoading(false);
+        }
+    }
     const handleChangeSearchData = async (value: any) => {
         setSearchKeyword(value.target.value);
         setDataRender([])
-        setSkip(0); // Reset page to 1 when search query changes
+        setSkip(0);
     };
     return (
         <>
@@ -60,7 +65,7 @@ const HomePage: React.FC = () =>  {
             <div className='container'>
                 <div className='search-wrapper'>
                     <form>
-                        <label>Search: </label>
+                        <label className='label-input'>Search: </label>
                         <input className='search-input' type='text' value={searchKeyword} onChange={(value: any) => handleChangeSearchData(value)}/>
                     </form>
                 </div>
@@ -68,11 +73,14 @@ const HomePage: React.FC = () =>  {
                     <table>
                         <thead>
                             <tr>
-                                <td>Index</td>
-                                <td>Title</td>
-                                <td>Description</td>
-                                <td>Price</td>
-                                <td>Discount</td>
+                                <td width={'5%'}>Index</td>
+                                <td width={'20%'}>Title</td>
+                                <td width={'5%'}>Image</td>
+                                <td width={'50%'}>Description</td>
+                                <td width={'5%'}>Discount</td>
+                                <td width={'5%'}>Price</td>
+                                <td width={'5%'}>Stock</td>
+                                <td width={'5%'}>Rating</td>
                             </tr>
                         </thead>
                         <tbody>
@@ -80,9 +88,12 @@ const HomePage: React.FC = () =>  {
                             <tr key={index}>
                                 <th>{index+1}</th>
                                 <th>{item.title}</th>
+                                <th><img alt={item.thumbnail} className='image-item' src={item.thumbnail}/></th>
                                 <th>{item.description}</th>
-                                <th>{item.price}</th>
                                 <th>{item.discountPercentage}</th>
+                                <th>{item.price}</th>
+                                <th>{item.stock}</th>
+                                <th>{item.rating}</th>
                             </tr>
                         ))}
                         </tbody>
@@ -91,7 +102,6 @@ const HomePage: React.FC = () =>  {
                         {dataRender.length === 0 && <p>No record founds</p>}
                 </div>
             </div>
-            {/*<footer>This source build by Bui Minh Phuong</footer>*/}
         </>
     );
 }
